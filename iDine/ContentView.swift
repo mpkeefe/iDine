70d085b67f3008839abbeec7f744c8b2452d67ae
Mark Paul Keefe <mark@maynebridge.com>
2020-02-22 12:16:22 -0500

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Keefe, Mark on 2/22/20.
+//  Copyright © 2020 Keefe, Mark. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
